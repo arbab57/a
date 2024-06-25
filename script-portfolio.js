112f01c21016b46c7b43c177740f8479df294657
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-06-25 18:15:32 +0500

    added project

--- script-portfolio.js
+++ script-portfolio.js
@@ -79,6 +79,14 @@ let projects = [
     main: "Digital Doorway: Animated Bootstrap Landing Page",
     desc: "This project demonstrates my ability to create effective landing pages that drive results. Whether you need a landing page for a product launch, campaign, or business promotion, I've got you covered!",
     view: "view Project"
+},
+{
+    href: "https://arbab57.github.io/portfolioreact/",
+    src:  "assets/projects/portfolioReact.png",
+    sub:  "React.JS",
+    main: "ComponentCraft: A React.js Portfolio",
+    desc: "Built using React.js, this portfolio demonstrates my proficiency in developing scalable, efficient, and interactive applications. The project features a curated selection of components and applications.",
+    view: "view Project"
 }
 
 
@@ -218,6 +226,7 @@ if (input === "next") {
     p0 = p0 + 6;
     p1 += 6;
     p2 += 6;
+    p3 += 6
     pageNum++;
     pageNum = pageNum == totalPages ? totalPages : pageNum;
 }
@@ -226,6 +235,7 @@ if (input === "prev") {
     p0 = p0 - 6;
     p1 -= 6;
     p2 -= 6;
+    p3 -= 6;
     pageNum--;
     pageNum = pageNum == 0 ? 1 : pageNum;  
 }
@@ -233,6 +243,7 @@ if (input === 1) {
     p0 = 0;
     p1 = 1;
     p2 = 2;
+    p3 = 3;
     pageNum = input
    
 }
@@ -240,6 +251,7 @@ if (input === 2) {
     p0 += 6;
     p1 += 6;
     p2 += 6;
+    p3 += 6;
    pageNum = input; 
 }
 
@@ -295,14 +307,14 @@ console.log(pageNum)
     if (pageNum === 2) {
         
        
-        conP4.classList.add("d-none")
+        
         conP5.classList.add("d-none")
         conP6.classList.add("d-none")
 
     } else {
         
         
-        conP4.classList.remove("d-none")
+        
         conP5.classList.remove("d-none")
         conP6.classList.remove("d-none")
     }
